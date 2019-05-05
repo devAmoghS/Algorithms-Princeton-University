7319dfae70ddca68e0864fbe26b1e458e98925a9
Amogh Singhal <devamoghs@gmail.com>
2019-05-05 18:28:27 +0530

    Create quick_union_lazy.py

---
+++ quick_union_lazy.py
@@ -0,0 +1,26 @@
+# Lazy implementation of the Quick Union algorithm
+# Problem: Dynamic Connectivity
+
+# ** Peformance
+# 1. is_connected: takes constant time - O(N)
+# 2. union: takes quadratic time - O(N)
+
+class QuickUnionUF:
+  def __init__(self, arr, size):
+    self.arr = [i for i in range(0, size)]
+
+  def root(self, i):
+    array = self.arr
+    while array[i] != i:
+      i = array[i]
+    return i
+
+  def is_connected(self, p, q):
+    return root(p) == root(q) 
+
+  def union(self, p, q):
+    array = self.arr
+    i = root(p)
+    j = root(q)
+
+    array[i] = j
